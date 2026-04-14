34908bf0e23622297e5262c7999ae3c079b8e19a
Lostigen <jimmy.lostigen@outlook.com>
2026-04-14 15:26:02 +0200

    Första versionen av MyFirstBlerp

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MyFirstBlerp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Be andvändaren om ett namn
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine() ?? "";
+
+        // Skriv ut en hälsning till användaren med namnet
+        Console.WriteLine($"Hello, and welcome, {name}!");
+        Console.ReadLine();
+    }
+}
