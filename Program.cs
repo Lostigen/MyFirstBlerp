7e7e0beb725aec1ba38ffb30013f101fe9788b96
Lostigen <jimmy.lostigen@outlook.com>
2026-04-21 14:52:11 +0200

    #

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace MyFirstBlerp;
+﻿using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace MyFirstBlerp;
 
 class Program
 {
@@ -8,10 +11,37 @@ class Program
 
         // Be andvändaren om ett namn
         Console.WriteLine("What is your name?");
-        string name = Console.ReadLine() ?? "";
+        string myName = Console.ReadLine() ?? "";
+        string mySurname = "Svensson";
+        int myAge = 50;
+        int MyShoeSize = 46;
 
         // Skriv ut en hälsning till användaren med namnet
-        Console.WriteLine($"Hello, and welcome, {name}!");
-        Console.ReadLine();
+        if (myName.ToLower() == "tony") Console.WriteLine("Fudge you, Tony!");
+        else Console.WriteLine($"Hello, and welcome, {myName}!");
+
+        // Man kan också skapa en instans av person-klassen med namn
+        Person individ1 = new Person(myName, mySurname, myAge, MyShoeSize); 
+         Console.ReadLine();
+         Console.WriteLine("Vi testar att skriva ut den nya individens objektvariabler.");
+         Console.WriteLine("Hej, {0} {1}! Du är {2} år gammal och du har skostorlek {3}.", individ1._firstName, individ1._lastName, individ1._age, individ1._shoeSize);
+    }
+
+
+}
+
+// Vi skapar person-klassen här
+public class Person
+{
+    // Objektvariabler
+    public string _firstName, _lastName;
+    public int _age, _shoeSize;
+
+    public Person(string firstName, string lastName, int age, int shoeSize)
+    {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._age = age;
+        this._shoeSize = shoeSize;
     }
 }
